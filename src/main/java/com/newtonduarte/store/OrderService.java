0ff495d4bb590e4b866796caf33f6b7403535852
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-04 15:28:13 -0300

    feat: add service and autowired annotation to be injected as a java bean

--- src/main/java/com/newtonduarte/store/OrderService.java
+++ src/main/java/com/newtonduarte/store/OrderService.java
@@ -1,8 +1,13 @@
 package com.newtonduarte.store;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
     private final PaymentService paymentService;
 
+    @Autowired
     public OrderService(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
