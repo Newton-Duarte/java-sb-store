c620550837f8549ef07d915e78aae51451a73f16
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-04 12:45:46 -0300

    feat: add order service

---
+++ src/main/java/com/newtonduarte/store/OrderService.java
@@ -0,0 +1,13 @@
+package com.newtonduarte.store;
+
+public class OrderService {
+    private final PaymentService paymentService;
+
+    public OrderService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    public void placeOrder(double amount) {
+        paymentService.processPayment(amount);
+    }
+}
